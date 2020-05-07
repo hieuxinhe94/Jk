73ed6678dd8459ed27d1fdcd6ec954898899e659
Pham Van Hieu <hieu.pham@niteco.se>
2020-05-07 17:15:25 +0700

    initial project

---
+++ Sources/Tools/ToolBusiness/CrawlerEngine.cs
@@ -0,0 +1,41 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Crawler.Business
+{
+    public class CrawlerEngine
+    {
+        private static async Task<string> GetResponseString(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+                var contents = await response.Content.ReadAsStringAsync();
+
+                return contents;
+            }
+        }
+
+        private static string DecodeBase64String(string content)
+        {
+            byte[] data = System.Convert.FromBase64String(content);
+            return System.Text.ASCIIEncoding.ASCII.GetString(data);
+        }
+
+        private static ICollection<string> ExtractReferLinks(string content)
+        {
+            string xpath = "//a[@href !='']";
+
+            var doc = new HtmlDocument();
+            doc.Load(content);
+
+            var nodes = doc.DocumentNode.SelectNodes(xpath);
+
+            return nodes.Select(t => t.InnerText).ToList();
+        }
+    }
+}
---
+++ Sources/Tools/ToolCore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ToolCore
+{
+    public class Class1
+    {
+    }
+}
